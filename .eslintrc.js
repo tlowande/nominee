c1209276571e73023d44dcd6e6c03adf91ed3311
Tamires <tamires.lowande@gmail.com>
2021-01-15 21:43:01 -0500

    setup

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+    jest: true,
+  },
+  settings: {
+    react: {
+      pragma: 'React',
+      version: 'detect',
+    }
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:react-hooks/recommended'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-hooks'],
+  rules: {
+    'comma-dangle': ['error', 'always-multiline'],
+    'eol-last': ['error', 'always'],
+    indent: ['error', 2],
+    'object-curly-spacing': ['error', 'always'],
+    quotes: ['error', 'single', { 'avoidEscape': true }],
+    semi: ['error', 'never'],
+    'react/display-name': 'warn',
+  },
+}
